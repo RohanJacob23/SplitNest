088858435e3bc68a3d77fec0c0761b3fb4fc7840
RohanJacob23 <rohankoshyjacob@gmail.com>
2025-07-27 13:23:19 +0530

    sm: ready for deploying

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,12 +1,28 @@
 import path from "node:path";
-import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [tailwindcss(), tanstackRouter({}), react(), cloudflare()],
+	plugins: [
+		tailwindcss(),
+		tanstackRouter({ autoCodeSplitting: true }),
+		react(),
+	],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					gsap: ["gsap"],
+					motion: ["motion/react"],
+					zod: ["zod/v4"],
+					lucidReact: ["lucide-react"],
+					orpc: ["@orpc/client", "@orpc/server", "@orpc/tanstack-query"],
+				},
+			},
+		},
+	},
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
